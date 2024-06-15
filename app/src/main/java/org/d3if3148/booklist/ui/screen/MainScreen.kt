8d277dd73217b34758b5d0a0cc4bded36dacb427
Fretty Simamora <purpieshomie@gmail.com>
2024-06-15 17:31:41 +0700

    commit 2

--- app/src/main/java/org/d3if3148/booklist/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3148/booklist/ui/screen/MainScreen.kt
@@ -6,15 +6,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -22,12 +27,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -39,6 +46,7 @@ import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import org.d3if3148.booklist.R
 import org.d3if3148.booklist.model.Book
+import org.d3if3148.booklist.network.ApiStatus
 import org.d3if3148.booklist.network.BookApi
 import org.d3if3148.booklist.ui.theme.BookListTheme
 
@@ -83,13 +91,45 @@ fun MainScreen() {
 fun ScreenContent(modifier: Modifier) {
     val viewModel: MainViewModel = viewModel()
     val data by viewModel.data
-    LazyVerticalGrid (
-        modifier = modifier
-            .fillMaxSize()
-            .padding(4.dp),
-        columns = GridCells.Fixed(2)
-    ) {
-        items(data){ ListItem(book = it)}
+    val status by viewModel.status.collectAsState()
+
+    when(status){
+        ApiStatus.LOADING -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        ApiStatus.SUCCESS -> {
+            LazyVerticalGrid (
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(4.dp),
+                columns = GridCells.Fixed(2)
+            ) {
+                items(data){
+                    ListItem(book = it)
+                }
+            }
+        }
+        ApiStatus.FAILED -> {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = stringResource(id = R.string.error))
+                Button(
+                    onClick = {viewModel.retrieveData()},
+                    modifier = Modifier.padding(top = 16.dp),
+                    contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+                ) {
+                    Text(text = stringResource(id = R.string.try_again))
+                }
+            }
+        }
     }
 }
 
@@ -107,6 +147,8 @@ fun ListItem(book: Book) {
             .build(),
             contentDescription = stringResource(id = R.string.gambar, book.judul),
             contentScale = ContentScale.Crop,
+            placeholder = painterResource(id = R.drawable.loading_img),
+            error = painterResource(id = R.drawable.broken_image),
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(4.dp)
--- app/src/main/java/org/d3if3148/booklist/ui/screen/MainViewModel.kt
+++ app/src/main/java/org/d3if3148/booklist/ui/screen/MainViewModel.kt
@@ -5,8 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import org.d3if3148.booklist.model.Book
+import org.d3if3148.booklist.network.ApiStatus
 import org.d3if3148.booklist.network.BookApi
 
 class MainViewModel: ViewModel() {
@@ -14,16 +16,22 @@ class MainViewModel: ViewModel() {
     var data = mutableStateOf(emptyList<Book>())
         private set
 
+    var status = MutableStateFlow(ApiStatus.LOADING)
+        private set
+
     init {
         retrieveData()
     }
 
-    private fun retrieveData(){
+    fun retrieveData(){
         viewModelScope.launch(Dispatchers.IO) {
+            status.value = ApiStatus.LOADING
             try {
                 data.value = BookApi.service.getBook()
+                status.value = ApiStatus.SUCCESS
             } catch (e: Exception){
                 Log.d("MainViewModel", "Failure: ${e.message}")
+                status.value = ApiStatus.FAILED
             }
         }
     }
